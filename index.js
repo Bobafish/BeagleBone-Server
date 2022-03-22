123b23d47149529e6bcf15878e03051a74fd1468
Jongwon An <87057515+Bobafish@users.noreply.github.com>
2022-03-21 18:08:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//install packages
+//incorporates the packages
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -6,14 +6,17 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const cv = require('opencv4nodejs');
 
-//connects to the video source (0 = defaultcam)
+//connects to the video source (0 = defaultcam) 
 //would need to create wCapI, wCapII, etc to create different sources of video
 const wCap = new cv.VideoCapture(0);
 
 //controls how quickly the video updates
 const fps = 1;
 
-//set size of the camera
+//setting the count of the names
+let count = 1;
+
+//sets the size of the camera
 wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 1280);
 wCap.set(cv.CAP_PROP_FRAME_WIDTH, 720);
 
@@ -39,6 +42,12 @@ setInterval(() => {
     io.emit('imageII', imageII);
     io.emit('imageIII', imageIII);
     io.emit('imageIV', imageIV);
+    //saves the image
+    cv.imwrite(`C:/Users/anjon/Desktop/testytest/imageI${count}.jpg`, frameI);
+    cv.imwrite(`C:/Users/anjon/Desktop/testytest/imageII${count}.jpg`, frameII);
+    cv.imwrite(`C:/Users/anjon/Desktop/testytest/imageIII${count}.jpg`, frameIII);
+    cv.imwrite(`C:/Users/anjon/Desktop/testytest/imageIV${count}.jpg`, frameIV);
+    count++;
 }, 1000/fps)
 
 // listens to localhost:3000
